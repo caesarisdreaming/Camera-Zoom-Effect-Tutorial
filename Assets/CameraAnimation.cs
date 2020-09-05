4d2543a518038394d4a7474e9ce307b287fbd141
Yazid Taki <46725845+Toucano@users.noreply.github.com>
2020-09-05 13:20:23 +0100

    Update CameraAnimation.cs

--- Assets/CameraAnimation.cs
+++ Assets/CameraAnimation.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class CameraAnimation : MonoBehaviour
 {
+    //The smaller those numbers are the quicker you will go from a zoom state to the other
+    [SerializeField] float endOfZoomInCursor = .001f; 
+    [SerializeField] float endOfZoomOutCursor = .05f;
 
+    //Target values for the zoom effect
     [SerializeField] float zoomInSpeed = 1f;
     [SerializeField] float zoomOutSpeed = 1f;
     [SerializeField] float targetZoomIn = .3f;
 
-    float initalZoom = 0f;
+    //Initial value for zoom out effect
+    float initalZoom = 0f;// I am just going to keep this typo after all :)
     float initialPosX;
     float initialPosY;
 
+    //State bool for controling the methods
     public static bool IsZoomedIn = false;
     public static bool IsZoomedOut = false;
 
+    //Cache reference
     Transform target;
     Camera cam;
 
 
     // Start is called before the first frame update
+    //Initializing our values
     void Start()
     {
         cam = Camera.main;
@@ -37,7 +45,7 @@ public class CameraAnimation : MonoBehaviour
             cam.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, target.position.x, Time.deltaTime * zoomInSpeed),
                 Mathf.Lerp(cam.transform.position.y, target.position.y, Time.deltaTime * zoomInSpeed), 
                 cam.transform.position.z);
-            if (cam.orthographicSize - targetZoomIn <= .001f)
+            if (cam.orthographicSize - targetZoomIn <= endOfZoomInCursor)
             {
                 IsZoomedIn = true;
             }
@@ -52,7 +60,7 @@ public class CameraAnimation : MonoBehaviour
             cam.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, initialPosX, Time.deltaTime * zoomOutSpeed),
                 Mathf.Lerp(cam.transform.position.y, initialPosY, Time.deltaTime * zoomOutSpeed),
                 cam.transform.position.z);
-            if (initalZoom - cam.orthographicSize <= .05f)
+            if (initalZoom - cam.orthographicSize <= endOfZoomOutCursor)
             {
                 IsZoomedIn = false;
                 IsZoomedOut = true;
